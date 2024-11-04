01312c09252f73641a3b2226d293d92524443164
nazimudheenti <nazimudheen@diginestsolutions.in>
2024-11-04 23:32:45 +0530

    latest commit for delete/update user

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import asyncHandler from 'express-async-handler'
-import { addUser, allUsers, loginUser, userProfile } from '../controller/authCntrl';
+import { addUser, allUsers, deleteUser, loginUser, userProfile, updateUser } from '../controller/authCntrl';
 
 const router = express.Router();
 
@@ -8,6 +8,8 @@ router.post('/register', asyncHandler(addUser));
 router.post('/login', asyncHandler(loginUser));
 router.get('/all-users', asyncHandler(allUsers));
 router.get('/:id', asyncHandler(userProfile));
+router.delete('/delete/:id', asyncHandler(deleteUser));
+router.put('/update/:id', updateUser)
 
 
 export default router;
